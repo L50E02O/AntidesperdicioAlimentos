9499d2a1d06fa4b18046ea3391dc22afe1480ebf
FortyTwo43 <neycraft244@gmail.com>
2025-06-01 17:46:48 -0500

    se arregló algo en el modulo de Vue

--- Vue/moduloAdmin/src/types/inventario.d.ts
+++
@@ -1,9 +0,0 @@
-import {Producto} from "./producto";
-
-export interface Inventario {
-    id_inventario: number;
-    fecha_actualizacion: string;
-    cantidad_total: number;
-    valor_inventario: number;
-    productos: Producto[]
-}
