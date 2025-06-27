b92a1e24e530122ac2c7e7abc6c527231b79d4d4
Leoh <leoanthonyholguinchavez@gmail.com>
2025-06-27 09:52:53 -0500

    Cambiar cliente supabase por rest de supabase

--- Angular/comerciante/src/app/features/layout/layout.route.ts
+++ Angular/comerciante/src/app/features/layout/layout.route.ts
@@ -1,6 +1,9 @@
 import {Routes} from '@angular/router';
-import { DashboardComponent } from './dashboard/dashboard.component';
+import { DashboardComponent } from './dashboard-general/dashboard.component';
+import { DashboardPedidoComponent } from './dashboard-pedido/dashboard-pedido.component';
+
 
 export const layoutRoutes: Routes = [
-    {path: 'dashboard', component: DashboardComponent}
+    {path: 'dashboard', component: DashboardComponent},
+    {path: 'dashboard-pedido', component: DashboardPedidoComponent}
 ]
